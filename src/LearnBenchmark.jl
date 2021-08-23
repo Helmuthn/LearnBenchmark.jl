408011b65a0c850d50e9fbd849f557b5c4a4469c
Helmuth Naumer <hnaumer2@illinois.edu>
2021-08-23 10:41:09 -0500

    Files generated by PkgTemplates
    PkgTemplates version: 0.7.18

---
+++ src/LearnBenchmark.jl
@@ -0,0 +1,5 @@
+module LearnBenchmark
+
+# Write your package code here.
+
+end
---
+++ test/runtests.jl
@@ -0,0 +1,6 @@
+using LearnBenchmark
+using Test
+
+@testset "LearnBenchmark.jl" begin
+    # Write your tests here.
+end
